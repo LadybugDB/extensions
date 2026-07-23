f2a54a4ed725cb87d780e604fa2e07d5ed3b04de
chiangchenghsin-hash <chiang.chenghsin@gmail.com>
2026-07-23 20:08:15 +0800

    feat: add LeidenFunction/LeidenAliasFunction declarations for LEIDEN community detection"

---
+++ src/include/function/leiden.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "function/function.h"
+
+namespace lbug {
+namespace algo_extension {
+
+struct LeidenFunction {
+    static constexpr const char* name = "LEIDEN";
+    static function::function_set getFunctionSet();
+};
+
+struct LeidenAliasFunction {
+    using alias = LeidenFunction;
+    static constexpr const char* name = "LE";
+};
+
+} // namespace algo_extension
+} // namespace lbug
