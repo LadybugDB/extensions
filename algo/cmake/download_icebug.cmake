33340b0970fd407c209fafb20e28519b0d741dbe
Arun Sharma <arun@sharma-home.net>
2026-08-29 17:51:04 -0700

    Update icebug to 13.2

--- algo/cmake/download_icebug.cmake
+++ algo/cmake/download_icebug.cmake
@@ -40,7 +40,7 @@ if (DEFINED ICEBUG_SOURCE_DIR)
     return()
 endif ()
 
-set(ICEBUG_VERSION "13.1" CACHE STRING "icebug release tag")
+set(ICEBUG_VERSION "13.2" CACHE STRING "icebug release tag")
 set(ICEBUG_VENDOR_DIR "${CMAKE_CURRENT_SOURCE_DIR}/vendor")
 
 if (APPLE)
